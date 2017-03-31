4d26f9bd1e5e7ef84607e4f7918723ab7a1aceab
Rena <renashinomiya@gmail.com>
2017-03-30 20:07:11 -0500

    Add basic genetic algorithm (untested)

---
+++ src/genetic.c
@@ -0,0 +1,172 @@
+/**********************************************************//**
+ * @file genetic.c
+ * @brief Implementation of general genetic algorithm.
+ * @version 1.0
+ * @author Alec Shinomiya
+ * @date April 2017
+ **************************************************************/
+
+// Standard library
+#include <stddef.h>     // size_t
+#include <stdbool.h>    // bool
+#include <stdlib.h>     // malloc
+#include <string.h>     // memcpy
+
+// This project
+#include "debug.h"      // eprintf
+#include "heap.h"       // HEAP
+#include "genetic.h"    // GENETIC, GENETIC_REQUEST
+
+/**********************************************************//**
+ * @brief Gets the entity associated with the given index.
+ * @param data: The GENETIC algorithm data.
+ * @param index: The index of the entity sought. This can be
+ * from 0 to the population size - 1.
+ * @return Pointer to the entity. This could be out of bounds
+ * on invalid arguments.
+ **************************************************************/
+static inline void *genetic_Entity(GENETIC *data, int index) {
+    return ((char *)data->entities) + index;
+}
+
+/**********************************************************//**
+ * @brief Gets the index of the given entity.
+ * @param data: The GENETIC algorithm data.
+ * @param entity: A valid pointer to an entity in the data.
+ * @return Index of the entity. This could be out of bounds
+ * on invalid arguments.
+ **************************************************************/
+static inline int genetic_Index(const GENETIC *data, const void *entity) {
+    ptrdiff_t split = (char *)entity - (char *)data->entities;
+    return (int)split / (int)data->entitySize;
+}
+
+/**********************************************************//**
+ * @brief Gets a pointer to the newborn entity at the given
+ * index.
+ * @param data: The GENETIC algorithm data.
+ * @param index: The index of the entity sought. This can be
+ * from 0 to the number of newborns - 1.
+ * @return Pointer to the entity.
+ **************************************************************/
+static inline void *genetic_Newborn(GENETIC *data, int index) {
+    return ((char *)data->newborn) + index;
+}
+
+/*============================================================*
+ * Creation function
+ *============================================================*/
+bool genetic_Create(GENETIC *data, const GENETIC_REQUEST *request) {
+    // Move request information over
+    data->entitySize = request->entitySize;
+    data->populationSize = request->populationSize;
+    data->random = request->random;
+    data->breed = request->breed;
+    data->fitness = request->fitness;
+    
+    // Allocates data for the genetic algorithm
+    data->entities = malloc(data->entitySize*data->populationSize);
+    if (!data->entities) {
+        eprintf("Failed to allocate entities array.\n");
+        return false;
+    }
+    
+    // Create the sorting heap
+    if (!heap_Create(&data->heap, data->populationSize)) {
+        eprintf("Failed to create organism heap.\n");
+        free(data->entities);
+        return false;
+    }
+    
+    // Create the newborn list
+    data->newborn = malloc(data->entitySize*(data->populationSize/2 + 1));
+    if (!data->newborn) {
+        eprintf("Failed to create newborn array.\n");
+        free(data->entities);
+        heap_Destroy(&data->heap);
+        return false;
+    }
+    
+    // Initial seed
+    for (int i = 0; i < data->populationSize; i++) {
+        data->random(genetic_Entity(data, i));
+    }
+    
+    // Unrelated initialization
+    data->best = NULL;
+    return true;
+}
+
+/*============================================================*
+ * Computes one generation
+ *============================================================*/
+void genetic_Generation(GENETIC *data) {
+    // Create a heap to sort the population by fitness.
+    for (int i = 0; i < data->populationSize; i++) {
+        heap_Push(&data->heap, i, data->fitness(genetic_Entity(data, i)));
+    }
+    
+    // Set the best individual
+    int bestIndex = -1;
+    heap_Top(&data->heap, &bestIndex);
+    data->best = genetic_Entity(data, bestIndex);
+    
+    // Decide what to do with organisms
+    // Use this formula for nBreed to ensure half the population
+    // gets paired up (multiple of 2).
+    int nBreed = 2*(data->populationSize/4);
+    
+    // Generate all the newborns
+    for (int n = 0; n < nBreed; n += 2) {
+        int motherIndex;
+        int fatherIndex;
+        heap_Pop(&data->heap, &motherIndex);
+        heap_Pop(&data->heap, &fatherIndex);
+        void *mother = genetic_Entity(data, motherIndex);
+        void *father = genetic_Entity(data, fatherIndex);
+        void *son = genetic_Newborn(data, n);
+        void *daughter = genetic_Newborn(data, n+1);
+        data->breed(mother, father, son, daughter);
+    }
+    
+    // Kill the remaining ones and place newborns in their place
+    for (int n = 0; n < nBreed; n++) {
+        int index;
+        heap_Pop(&data->heap, &index);
+        memcpy(genetic_Entity(data, index), genetic_Newborn(data, n), data->entitySize);
+    }
+    
+    // If any stragglers left on the heap, just randomize them to keep
+    // the same population size.
+    while (heap_Size(&data->heap) > 0) {
+        int index;
+        heap_Pop(&data->heap, &index);
+        data->random(genetic_Entity(data, index));
+    }
+}
+
+/*============================================================*
+ * Useful solver algorithm
+ *============================================================*/
+int genetic_Solve(GENETIC *data, float fitness, int timeout) {
+    int generation = 0;
+    while (generation < timeout || timeout == TIMEOUT_NONE) {
+        genetic_Generation(data);
+        generation++;
+        if (data->fitness(data->best) <= fitness) {
+            return generation;
+        }
+    }
+    return timeout;
+}
+
+/*============================================================*
+ * Destructor
+ *============================================================*/
+void genetic_Destroy(GENETIC *data) {
+    heap_Destroy(&data->heap);
+    free(data->entities);
+    free(data->newborn);
+}
+
+/*============================================================*/
---
+++ src/genetic.h
@@ -0,0 +1,129 @@
+/**********************************************************//**
+ * @file genetic.h
+ * @brief Declaration of general genetic algorithm.
+ * @version 1.0
+ * @author Alec Shinomiya
+ * @date April 2017
+ **************************************************************/
+
+#ifndef _GENETIC_H_
+#define _GENETIC_H_
+
+// Standard library
+#include <stddef.h>     // size_t
+#include <stdbool.h>    // bool
+
+// This project
+#include "heap.h"       // HEAP
+
+/// @brief Value used for specifying that a genetic algorithm
+/// can proceed for infinite generations.
+#define TIMEOUT_NONE 0
+
+/**********************************************************//**
+ * @typedef RANDOM_FUNCTION
+ * @brief Generates a random entity.
+ * @param entity: The location the data is stored.
+ **************************************************************/
+typedef void (*RANDOM_FUNCTION)(void *entity);
+
+/**********************************************************//**
+ * @typedef BREEDING_FUNCTION
+ * @brief Breeds two entities and creates two children.
+ * @param mother: The first parent.
+ * @param father: The second parent.
+ * @param son: The first child generated.
+ * @param daughter: The second child generated.
+ **************************************************************/
+typedef void (*BREEDING_FUNCTION)(const void *mother, const void *father, void *son, void *daughter);
+
+/**********************************************************//**
+ * @typedef FITNESS_FUNCTION
+ * @brief Get the fitness of the organism in any order.
+ * @param entity: The entity to evaluate.
+ * @return The fitness (smaller numbers are more fit).
+ **************************************************************/
+typedef float (*FITNESS_FUNCTION)(const void *entity);
+
+/**********************************************************//**
+ * @struct GENETIC_REQUEST
+ * @brief Stores all the information required as user input
+ * to construct a GENETIC structure.
+ **************************************************************/
+typedef struct {
+    // Individuals
+    size_t entitySize;          ///< The size of each organism's data.
+    int populationSize;         ///< The number of entities in the population.
+    
+    // Functions
+    RANDOM_FUNCTION random;     ///< Generates a random entity.
+    BREEDING_FUNCTION breed;    ///< Breeds two entities.
+    FITNESS_FUNCTION fitness;   ///< Gets the fitness of the entity.
+} GENETIC_REQUEST;
+
+/**********************************************************//**
+ * @struct GENETIC
+ * @brief Stores all information related to solving a problem
+ * using a genetic algorithm.
+ **************************************************************/
+typedef struct {
+    // Individuals
+    size_t entitySize;          ///< The size of each organism's data.
+    int populationSize;         ///< The number of entities in the population.
+    
+    // Functions
+    RANDOM_FUNCTION random;     ///< Generates a random entity.
+    BREEDING_FUNCTION breed;    ///< Breeds two entities.
+    FITNESS_FUNCTION fitness;   ///< Gets the fitness of the entity.
+    
+    // Storage information
+    void *entities;             ///< The actual creature data stored in any order.
+    HEAP heap;                  ///< Heap used to sort organisms.
+    void *newborn;              ///< List used to capture all the newborn creatures.
+    const void *best;           ///< The best individual in the population.
+} GENETIC;
+
+/**********************************************************//**
+ * @brief Initializes a random population for the given
+ * genetic algorithm configuration.
+ * @param data: Storage location for algorithm data.
+ * @param request: User-specified algorithm parameters.
+ * @return Whether the creation suceeded.
+ **************************************************************/
+extern bool genetic_Create(GENETIC *data, const GENETIC_REQUEST *request);
+
+/**********************************************************//**
+ * @brief Get the best individual in the population.
+ * @param data: Genetic algorithm data.
+ * @return Pointer to the best entity, or NULL if invalid.
+ **************************************************************/
+static inline const void *genetic_Best(const GENETIC *data) {
+    return data->best;
+}
+
+/**********************************************************//**
+ * @brief Runs one generation of the genetic algorithm.
+ * @param data: Algorithm data.
+ **************************************************************/
+extern void genetic_Generation(GENETIC *data);
+
+/**********************************************************//**
+ * @brief Runs the algorithm to completion.
+ * @param data: Algorithm configuration.
+ * @param fitness: The minimum desired fitness of the best
+ * indivual. This will run until an individual with fitness
+ * less than or equal to this value is found. Must be at least 0.
+ * @param timeout: The maximum number of generations to run,
+ * or TIMEOUT_NONE if infinite is desired.
+ * @return The number of generations required.
+ **************************************************************/
+extern int genetic_Solve(GENETIC *data, float fitness, int timeout);
+
+/**********************************************************//**
+ * @brief Destroy the algorithm data.
+ * @param data: Algorithm data to get rid of.
+ **************************************************************/
+extern void genetic_Destroy(GENETIC *data);
+
+/*============================================================*/
+#endif // _GENETIC_H_
